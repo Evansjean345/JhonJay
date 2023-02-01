997321e4f603844864a77128cc7a6ceff89aad34
Evansjean345 <Evansjean345@gmail.com>
2023-02-01 16:06:11 +0000

    resolve 404 error

--- src/layouts/Tab.jsx
+++ src/layouts/Tab.jsx
@@ -39,14 +39,7 @@ export default function Tab() {
 
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
-              <div key={product.id} className="group relative">
-                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-                  <iframe
-                    src={product.Src}
-                    className="h-full w-full lg:h-full lg:w-full rounded-xl"
-                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen
-                  />
-                </div>
+              <iframe src={product.Src} key={product.id} className="group relative h-[330px] w-full rounded-xl">
                 <div className="mt-4 flex justify-between">
                   <div>
                     <h3 className="text-sm text-gray-700 font-bold">
@@ -58,7 +51,7 @@ export default function Tab() {
                     </p>
                   </div>
                 </div>
-              </div>
+              </iframe>
             ))}
           </div>
         </div>
